7cd0eef2217e9d188edaac98ffc87500fc31b234
karinadl <150309611+karinadl@users.noreply.github.com>
2024-04-08 22:33:47 -0600

    Update database.sql

--- db/database.sql
+++ db/database.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE IF NOT EXISTS universidad;
-USE universidad;
+-- Crear la base de datos universidad
+CREATE DATABASE universidad;
 
 -- Tabla para almacenar las carreras
 CREATE TABLE carreras (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   creditos INT,
   UNIQUE(nombre)
@@ -11,7 +11,7 @@ CREATE TABLE carreras (
 
 -- Tabla para almacenar las materias
 CREATE TABLE materias (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   creditos INT,
   carrera_id INT,
@@ -20,15 +20,15 @@ CREATE TABLE materias (
 
 -- Tabla para almacenar las especialidades
 CREATE TABLE especialidades (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   carrera_id INT,
   FOREIGN KEY (carrera_id) REFERENCES carreras(id) ON DELETE CASCADE 
 );
 
 -- Tabla para almacenar las materias de especialidad
-CREATE TABLE materias_especialid (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE materias_especialidad (
+  id SERIAL PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   carrera_id INT NOT NULL,
   especialidad_id INT NOT NULL,
@@ -36,4 +36,3 @@ CREATE TABLE materias_especialid (
   FOREIGN KEY (especialidad_id) REFERENCES especialidades(id) ON DELETE CASCADE -- Establece la relación con la tabla de especialidades
 );
 
-DESCRIBE carrera;
